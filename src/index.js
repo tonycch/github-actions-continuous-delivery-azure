303a77cec1117d3b3ac84cbee6322ba2a665b6d1
Tony Cheung <tonycch@github.com>
2020-11-19 08:14:58 +0800

    rename to player A B

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import Game from './game.js'
 
 let p1, p2
 while (!p1) {
-  p1 = window.prompt('Who is player 1?')
+  p1 = window.prompt('Who is player A?')
 }
 
 while (!p2 && p1 !== p2) {
   p2 = window.prompt(p1 === p2
     ? `Please enter a different name than ${p1}.`
-    : 'Who is player 2?')
+    : 'Who is player B?')
 }
 
 window.onload = () => {
